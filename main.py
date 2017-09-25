7dff6b7766c87de06e566e8e779701d1d268f348
Mingxing Qian <mxqian2000@yahoo.com>
2017-09-24 23:26:12 -0500

    web caesar

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 
 
+from flask import Flask, request
+from caesar import rotate_string
+
 <!DOCTYPE html>
+
 <html>
-<style>
-form {
-      background-color: lightgrey;
-      padding: 20px;
-      margin: 20px;
-      width: 540px;
-      font: 16px sans-serif;
-      border-radius: 10px;
-}
-textarea {
-         margin: 10px 0;
-         width: 540px;
-         height: 120px;
-}
-</style>
-<body>
+    <head>
+        <style>
+            form {
+                 background-color: lightgrey;
+                 padding: 20px;
+                 margin: 20px;
+                 width: 540px;
+                 font: 16px sans-serif;
+                 border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="/action_page.php" method="post">
+            <div>
+                <label for = "rot">Rotate by:</label>
+                <input type = "text" id="rot" value = "0" placeholder = "0">
+                 <p class = "Error"> </p>
+            </div>  
+            <p>
+               <label for="letters"></label>
+                <textarea type="text"  id="letters" required></textarea>   
+            </p>
+            <div>  
+               <label for="new_letters"></label>
+               <input type="submit" id = "new_letters" value = "Submit query"/>
+            </div>
+            '{letters} was rotated by {value} to get {new_letters}'.format_form(Default(letters='type', value = "type", new_letters = "submit_result"))
+        </form>
 
-<form action="/action_page.php" method = "post" style="border:1px solid #ccc">
-  <div>
-    <label for = "rot">Rotate by:</label>
-    <input name="rot" value = "0" placeholder = "0" type = "text">
-    <p class = "Error"> </p>
-  </div>  
-   <p>
-    <textarea type="text"  name="text" required></textarea>   
-   </p>
-  <div>   
-    <input type="submit" value = "Submit query" />
-  </div>
-</form>
 
-</body>
-</html>
+
+@app.route ("/", methods=['POST'])
